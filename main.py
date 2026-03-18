026c5403542cde81b933672278ad19732d72b1f7
chronicarkmods <coachbuzz44@gmail.com>
2026-03-18 11:12:48 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,10 @@ import os
 from discord.ext import commands
 from flask import Flask
 from threading import Thread
+intents = discord.Intents.default()
+intents.message_content = True
 
+bot = commands.Bot(command_prefix="!", intents=intents)
 bot = commands.Bot(command_prefix="!")
 
 @bot.event
